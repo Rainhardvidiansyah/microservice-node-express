9a3947d30638d6bef5069716204561ba6d015841
Rainhardvidiansyah <rainhardvidians@gmail.com>
2024-10-29 01:43:51 +0700

    add images schema

--- src/model/article.js
+++ src/model/article.js
@@ -1,13 +1,22 @@
 const mongoose = require('mongoose');
 
 
+const imageSchema = mongoose.Schema({
+    URLSearchParams: String,
+    caption: String,
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: Date
+},
+{ _id: false });
+
+
 const article = mongoose.Schema({
     title: String,
     author: {
             type: mongoose.Types.ObjectId,
             ref: 'User'
     },
-    images: [{Url: String, Name: String}],
+    images: [imageSchema],
     content: {type: String, required: true},
     tag: {type: String, required: true},
     createdAt: { type: Date, default: Date.now },
