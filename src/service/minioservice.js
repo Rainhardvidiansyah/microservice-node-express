2b91b5bd978f8e3cc77032a3ad2052f5f407ff86
Rainhardvidiansyah <rainhardvidians@gmail.com>
2024-11-01 01:14:00 +0700

    create minio service

---
+++ src/service/minioservice.js
@@ -0,0 +1,47 @@
+const Minio = require('minio');
+require('dotenv').config();
+
+let bucketName = process.env.MINIO_BUCKET;
+let minioAccessKey = process.env.MINIO_ACCESS_KEY;
+let minioSecretKey = process.env.MINIO_SECRET_KEY;
+let port = process.env.MINIO_PORT;
+
+const minioClient = new Minio.Client({
+    endPoint: 'localhost',
+    port: port,
+    useSSL: false,
+    accessKey: minioAccessKey,
+    secretKey: minioSecretKey
+
+});
+
+const getObject = (objectName) => {
+    return new Promise((resolve, reject) => {
+        minioClient.getObject(bucketName, objectName, (err, dataStream) => {
+            if (err) {
+                return reject(err);
+            }
+
+            let chunks = [];
+            dataStream.on('data', (chunk) => {
+                chunks.push(chunk);
+            });
+
+            dataStream.on('end', () => {
+                const buffer = Buffer.concat(chunks);
+                resolve(buffer); 
+            });
+
+            dataStream.on('error', (err) => {
+                reject(err);
+            });
+        });
+    });
+};
+
+
+//putObject(bucketName, objectName, stream, size, metaData[, callback])
+
+//getObject(bucketName, objectName, stream, size, metaData);
+
+module.exports = minioClient;
