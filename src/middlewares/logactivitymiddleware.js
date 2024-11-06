89e0f2437c7082aa5c8f784f51604416cecc3ff9
Rainhardvidiansyah <rainhardvidians@gmail.com>
2024-11-06 23:22:55 +0700

    edit and fixing bug in log activity middleware

--- src/middlewares/logactivitymiddleware.js
+++ src/middlewares/logactivitymiddleware.js
@@ -1,4 +1,3 @@
-const { MongoNotConnectedError } = require('mongodb');
 const logActivity = require('../model/logactivity');
 const userService = require('../service/userservice');
 
@@ -7,12 +6,19 @@ const userService = require('../service/userservice');
 const getAndSaveUserActivity = async (req, res, next) => {
 
     try {
-        const userId = req.id;
+        const userId = req.userId; //req.userId is from variable that I created in authMiddle, specifically from verifyToken method
+        if(!userId){
+            return res.status(401).json({'message': 'user doesn\'t have permission to access this endpoint'});
+        }
+
         const user = await userService.findUserById(userId);
+
         if(!user) {
             return res.status(401).json({ message: 'Unauthorized' });
         }
-//userId, endpoint, method, path, ip
+
+
+        //userId, endpoint, method, path, ip
         const activity = new logActivity({
             userId: user._id,
             endpoint: req.getOriginalUrl,
@@ -22,6 +28,7 @@ const getAndSaveUserActivity = async (req, res, next) => {
         });
 
         console.log(`Get data user id ${activity.userId} from log activity middleware function`);
+        console.log(' ');
         
         await activity.save();
         
